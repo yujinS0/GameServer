27fa6219c2ab5251af5653ef5dec3f49048f0388
U_Zin <930yujin@gmail.com>
2024-05-08 02:03:04 +0900

    [ADD] roomManager

--- SocketServer/OmokServer/OmokServer/PKHandler.cs
+++ SocketServer/OmokServer/OmokServer/PKHandler.cs
@@ -9,12 +9,13 @@ public class PKHandler
     public static Action<MemoryPackBinaryRequestInfo> DistributeInnerPacket;
 
     protected UserManager _userMgr = null;
+    public RoomManager _roomMgr = null;
 
-
-    public void Init(UserManager userMgr)
+    public void Init(UserManager userMgr, RoomManager roomMgr)
     {
         this._userMgr = userMgr;
-    }           
-            
-    
+        this._roomMgr = roomMgr;
+    }
+
+
 }
