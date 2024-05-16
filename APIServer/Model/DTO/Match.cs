fe8f21969af6a1b3bec595846bf0f733b69030e5
U_Zin <930yujin@gmail.com>
2024-05-16 10:01:32 +0900

    [ADD] Match API

---
+++ APIServer/Model/DTO/Match.cs
@@ -0,0 +1,28 @@
+﻿using APIServer.Models.GameDB;
+using System.ComponentModel.DataAnnotations;
+
+namespace APIServer.Model.DTO
+{
+    public class MatchRequest
+    {
+        [Required] public string Email { get; set; }
+    }
+
+    public class MatchResponse
+    {
+        [Required] public ErrorCode Result { get; set; } = ErrorCode.None;
+    }
+
+    public class MatchCompleteResponse
+    {
+        [Required] public ErrorCode Result { get; set; } = ErrorCode.None;
+        [Required] public int Success { get; set; } // 매칭 성공하면 1
+        [Required] public string Email { get; set; }
+        [Required] public int RoomNum { get; set; }
+    }
+    public class MatchCancelResponse
+    {
+        [Required] public ErrorCode Result { get; set; } = ErrorCode.None;
+        [Required] public string Message { get; set; }
+    }
+}
